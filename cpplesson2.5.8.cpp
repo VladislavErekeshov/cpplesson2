549dae89a94feb2aa5e2c30df42325639d7e8066
Владислав <tommysyching@gmail.com>
2021-02-24 00:13:41 +0300

    Add files via upload

---
+++ cpplesson2.5.8.cpp
@@ -0,0 +1,30 @@
+﻿/*Вводится одно натуральное число, не более 31. Напишите программу, которая делает то же самое, что и в примерах входных и выходных данных.
+
+Sample Input 1:
+
+18
+Sample Output 1:
+
+10010
+Sample Input 2:
+
+31
+Sample Output 2:
+
+11111
+Sample Input 3:
+
+10
+Sample Output 3:
+
+01010
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    cout << a / 16 % 2 << a / 8 % 2 << a / 4 % 2 << a / 2 % 2 << a % 2;
+}
