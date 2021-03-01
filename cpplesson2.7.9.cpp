eb239806fc2840cfff20032a21958947f86f3a82
Владислав <tommysyching@gmail.com>
2021-03-01 19:23:32 +0300

    Add files via upload

---
+++ cpplesson2.7.9.cpp
@@ -0,0 +1,25 @@
+﻿/*Преобразуйте последовательность из восьми чисел, состоящую только из 1 и 0 в другую, как показано в примере.
+
+Sample Input 1:
+
+0 1 0 1 1 0 1 1
+Sample Output 1:
+
+91
+Sample Input 2:
+
+1 0 0 1 0 1 1 0
+Sample Output 2:
+
+150
+*/
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, d, e, a1, a2, a3;
+    cin >> a1 >> a2 >> a3 >> a >> b >> c >> d >> e;
+    cout << a1 * 128 + a2 * 64 + a3 * 32 + a * 16 + b * 8 + c * 4 + d * 2 + e;
+}
