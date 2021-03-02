6cdc3a579792bcce46307013b89235e864f62b64
Владислав <tommysyching@gmail.com>
2021-03-02 15:57:23 +0300

    Add files via upload

---
+++ cpplesson2.8.4.cpp
@@ -0,0 +1,28 @@
+﻿/*Напишите программу, которая запрашивает с клавиатуры одно натуральное число -- количество информации в Байтах, и выводит в первой строке соответствующее ему целое количество КилоБайт, а во второй строке, количество оставшихся Байт.
+
+Sample Input 1:
+
+2700
+Sample Output 1:
+
+2 KByte
+652 Byte
+Sample Input 2:
+
+1
+Sample Output 2:
+
+0 KByte
+1 Byte
+*/
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    cout << x / 1024 << " KByte" << endl;
+    cout << x % 1024 << " Byte";
+}
