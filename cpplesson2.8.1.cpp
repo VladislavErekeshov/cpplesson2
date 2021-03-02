bc7e9624a9e505e0674fb9a21177d2602b36a772
Владислав <tommysyching@gmail.com>
2021-03-02 15:44:43 +0300

    Add files via upload

---
+++ cpplesson2.8.1.cpp
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая запрашивает с клавиатуры одно натуральное число -- количество информации в битах, и выводит соответствующее ему целое количество байт.
+
+Sample Input 1:
+
+32
+Sample Output 1:
+
+4
+Sample Input 2:
+
+1024
+Sample Output 2:
+
+128
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    cout << x / 8;
+    return 0;
+}
